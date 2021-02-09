95382875b125a068165c54ec852a7fba88200574
Abigail Velazquez <aby1907@hotmail.com>
2021-02-09 11:04:18 -0700

    Initial Commit

---
+++ Algoritmos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algoritmos
+//
+//  Created by Aby Velazquez on 09/02/21.
+//  Copyright © 2021 Abi Velazquez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
