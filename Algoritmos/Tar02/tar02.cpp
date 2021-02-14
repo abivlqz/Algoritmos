439a10f0379c5add5a377a2eca87b7ffda55d847
Abigail Velazquez <aby1907@hotmail.com>
2021-02-13 17:20:36 -0700

    Tarea2 terminada

---
+++ Algoritmos/Tar02/tar02.cpp
@@ -0,0 +1,47 @@
+//
+//  tar02.cpp
+//  Algoritmos
+//
+//  Abigail Guadalupe Velazquez Sanchez A01566592 Grupo1
+//  Creado el 13/02/2021
+/*  Este programa solicita al usuario un tipo de dato string que contenga varios ceros y unos, y despliega en pantalla la longitud final del string si se eliminan n-veces los pares de unos y ceros ya sea 10 o 01
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Funcion findLong que recibe de parametro un string con numeros 1s y 0s junto con el tamaño del string
+// y despliega en pantalla el numero de caracteres que sobran despues de eliminar cualquier
+// uno y cero que esten juntos
+void findLong(string str, int n){
+    int numCeros = 0;
+    int numOnes = 0;
+    // Cuenta el numero de 0s y el numero de 1s en el string
+    for (int i = 0; i<n; i++) {
+        if (str[i] == '0')
+            numCeros++;
+        else
+            numOnes++;
+    }
+    // Imprime la diferencia, que es igual a la cantidad de numeros resultantes
+    // despues de la operacion de eliminar 0s y 1s que esten consecutivos
+    if (numCeros>=numOnes)
+        cout<<numCeros-numOnes<<endl;
+    else
+        cout<<numOnes-numCeros<<endl;
+}
+
+// Programa principal
+int main(){
+    string str;
+    int n;
+    cout<<"Ingrese la longitud del string desde 1 a 200,000"<<endl;
+    cin>>n;
+    cout<<"Ingrese su string :"<<endl;
+    cin>>str;
+    findLong(str,n);
+    
+    return 0;
+}
